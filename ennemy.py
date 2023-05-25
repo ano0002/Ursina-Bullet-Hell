d49ee6b4b7e2a8786bca632c0698c31c45687682
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-05-25 13:28:11 +0200

    Added a laser Ennemy

--- ennemy.py
+++ ennemy.py
@@ -22,6 +22,7 @@ class Ennemy(Entity):
         self.total_alive = 0
         self.lives = lives
         self.ondeath = on_death
+        self.bullets_shot = []
         
     def update(self):
         if self.lives > 0:
@@ -33,18 +34,24 @@ class Ennemy(Entity):
             
             if hasattr(self,"custom_update"):
                 self.custom_update()
-        
+
+            for bullet in self.bullets_shot:
+                if bullet.available:
+                    self.bullets_shot.remove(bullet)
+            
         else:
             self.die()
             destroy(self)
 
-    def shoot(self,angle=0):
+    def shoot(self,angle=0, speed=1):
         for bullet in self.bullets:
             if bullet.available:
                 bullet.team = self.team
                 bullet.position = Vec2(self.position.x/(32*camera.aspect_ratio), self.position.y /32)
-                bullet.velocity = Vec2(math.sin(math.radians(self.rotation_z+angle)), math.cos(math.radians(self.rotation_z+angle))) * 0.2
+                bullet.velocity = Vec2(math.sin(math.radians(self.rotation_z+angle)), math.cos(math.radians(self.rotation_z+angle))) * 0.2 * speed
+                self.bullets_shot.append(bullet)
                 break
+    
     
     def shot(self):
         for bullet in self.bullets:
@@ -94,14 +101,18 @@ class QuadrupleSpiralEnnemy(Ennemy):
         self.rotation_z += self.SPEED*5*time.dt
 
 class MachineGunEnnemy(Ennemy):
-    def __init__(self, bullets,speed = 5,fire_rate= 5, **kwargs):
+    def __init__(self, bullets,speed = 5,fire_rate= 25, **kwargs):
         super().__init__(bullets, speed = speed,fire_rate = fire_rate, **kwargs)
     
     def custom_update(self):
         if self.total_alive > 1/self.fire_rate:
-            self.shoot()
+            self.shoot(random.random() * 360,0.2)
             self.total_alive = 0
-
+        
+        for bullet in self.bullets_shot:
+            if distance_2d(bullet.get_world_position(),self.position) > 4:
+                bullet.available = True
+        
 class AimerEnnemy(Ennemy):
     def __init__(self, bullets,targets,speed = 5,fire_rate= 1, **kwargs):
         super().__init__(bullets, speed = speed,fire_rate = fire_rate, **kwargs)
@@ -147,6 +158,20 @@ class PatrolEnnemy(Ennemy):
             self.current_waypoint = (self.current_waypoint+1)%len(self.waypoints)
             self.animate_position(self.waypoints[self.current_waypoint],duration=1/self.SPEED,curve=curve.linear)
 
+class LaserEnnemy(Ennemy):
+    def __init__(self, bullets,speed = 5,fire_rate= 1, **kwargs):
+        super().__init__(bullets, speed = speed,fire_rate = fire_rate, **kwargs)
+        self.last_bullet = 0
+    
+    def custom_update(self):
+        if self.total_alive > 1/self.fire_rate:
+            if self.total_alive-self.last_bullet > 0.01:
+                self.shoot(speed=5)
+                self.last_bullet = self.total_alive
+            if self.total_alive > 1/self.fire_rate+0.6:
+                self.total_alive = 0
+                self.last_bullet = 0
+        self.rotation_z += self.SPEED*5*time.dt
 
 class Boss(Ennemy):
     def __init__(self, bullets, waypoints, targets,speed =0.2,fire_rate= 1, **kwargs):
@@ -177,9 +202,20 @@ class Boss(Ennemy):
             self.current_waypoint = (self.current_waypoint+1)%len(self.waypoints)
             self.animate_position(self.waypoints[self.current_waypoint],duration=1/self.SPEED,curve=curve.linear)
 
+    
+    def shot(self):
+        for bullet in self.bullets:
+            if not bullet.available:
+                if bullet.team != self.team:
+                    if distance_2d(bullet.get_world_position(),self.position) < 0.8:
+                        bullet.available = True
+                        return True
+        return False
+
     def die(self):
         self.life_bar.enabled = False
         super().die()
+
 
 if __name__ == "__main__":
 
@@ -196,15 +232,18 @@ if __name__ == "__main__":
     bullets = [Bullet(Vec2(1,1),Vec2(0,0)) for _ in range(1000)]
 
     p1 = KeyboadPlayer(bullets,team=0, lives=float('inf'))
-    p2 = ControllerPlayer(bullets, team=0, lives=float('inf'))
+    #p2 = ControllerPlayer(bullets, team=0, lives=float('inf'))
+    
+    players = [p1]
     
     MachineGunEnnemy(bullets, position=Vec2(-5,5))
     SpiralEnnemy(bullets, position=Vec2(0,5))
     DoubleSpiralEnnemy(bullets, position=Vec2(5,5))    
     QuadrupleSpiralEnnemy(bullets, position=Vec2(10,5))
-    AimerEnnemy(bullets, (p1,p2), position=Vec2(-10,5))
-    PatrolEnnemy(bullets, [Vec2(-10,0),Vec2(-10,10),Vec2(10,10),Vec2(10,0)], (p1,p2), position=Vec2(0,0))
-    Boss(bullets, [Vec2(-10,-5),Vec2(10,-5)], (p1,p2), position=Vec2(0,-5), lives=50)
+    AimerEnnemy(bullets, players, position=Vec2(-10,5))
+    PatrolEnnemy(bullets, [Vec2(-10,0),Vec2(-10,10),Vec2(10,10),Vec2(10,0)],players, position=Vec2(0,0))
+    Boss(bullets, [Vec2(-10,-5),Vec2(10,-5)],players, position=Vec2(0,-5), lives=50)
+    LaserEnnemy(bullets, position=Vec2(0,10))
 
 
     def update():
